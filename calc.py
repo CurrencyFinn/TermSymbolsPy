10bdef06a7e9d6e6c874fc67c04557a9b26ab9bf
Finn <currencyfinn@gmail.com>
2023-05-23 17:42:55 +0200

    all electrons in orbitals

--- calc.py
+++ calc.py
@@ -20,53 +20,57 @@ def calcPossibleConfig(m,vElectron, limit):
         return reOrderedMicroState
 
     i=0 
-    #timesLooped = 0
     previousi = 0
     iCounter = 0
     while i < nbConfiguration:
-        #print(i)
-        specificMicroState = [[[],[]] for _ in range(m)]
-        pluggedInElectrons = 0
-        for j in range(0,m):
-            k = 0
-            while k < 2: # Only 2 avialable spots every orbital in microstate
-                inputInteger = randint(0,2)
-                inputElectron = possibleconfig[inputInteger]
-                if pluggedInElectrons == vElectron: # Check if the maximum amount of vElectrons in put in
-                    specificMicroState[j][k] = 0
-                    k+=1
-                    continue
-                if k==1:
-                    while specificMicroState[j][0] == inputElectron:
-                        inputInteger = randint(0,2)
-                        inputElectron = possibleconfig[inputInteger]
-                    if inputElectron == specificMicroState[j][0]*-1:
-                        pluggedInElectrons+=1
-                        specificMicroState[j][0] = 0.5
-                        specificMicroState[j][1] = -0.5
+        def createMicroStates():
+            specificMicroState = [[[],[]] for _ in range(m)]
+            pluggedInElectrons = 0
+            for j in range(0,m):
+                k = 0
+                
+                while k < 2: # Only 2 avialable spots every orbital in microstate
+                    inputInteger = randint(0,2)
+                    inputElectron = possibleconfig[inputInteger]
+                    if pluggedInElectrons == vElectron: # Check if the maximum amount of vElectrons in put in
+                        specificMicroState[j][k] = 0
                         k+=1
                         continue
-                    if specificMicroState[j][0] == 0 and inputElectron != 0:
-                        pluggedInElectrons+=1
-                        specificMicroState[j][0] = inputElectron
-                        specificMicroState[j][1] = 0
-                        k+=1
-                        continue
+                    if k==1:
+                        while specificMicroState[j][0] == inputElectron: # pauli exclusion
+                            inputInteger = randint(0,2)
+                            inputElectron = possibleconfig[inputInteger]
+                        if inputElectron == specificMicroState[j][0]*-1:
+                            pluggedInElectrons+=1
+                            specificMicroState[j][0] = 0.5
+                            specificMicroState[j][1] = -0.5
+                            k+=1
+                            continue
+                        if specificMicroState[j][0] == 0 and inputElectron != 0:
+                            pluggedInElectrons+=1
+                            specificMicroState[j][0] = inputElectron
+                            specificMicroState[j][1] = 0
+                            k+=1
+                            continue
+                        else:
+                            if inputElectron != 0:
+                                pluggedInElectrons+=1
+                            specificMicroState[j][1] = inputElectron
+
                     else:
                         if inputElectron != 0:
                             pluggedInElectrons+=1
-                        specificMicroState[j][1] = inputElectron
-
-                else:
-                    if inputElectron != 0:
-                        pluggedInElectrons+=1
-                    specificMicroState[j][k] = inputElectron
-                    k+=1 
-                    continue
-
+                        specificMicroState[j][k] = inputElectron
+                        k+=1
+                        continue
+            return specificMicroState, pluggedInElectrons
+            
+        datCreatedMicroState = createMicroStates()
+        while datCreatedMicroState[1] != vElectron: # ensuring all electrons are in the orbitals
+            datCreatedMicroState = createMicroStates()
 
         #timesLooped += 1
-        result = any(all(all(is_close(a, b) for a, b in zip(subarray_2d, input_array_2d)) for subarray_2d, input_array_2d in zip(subarray, specificMicroState)) for subarray in totalMicroStates if subarray) and specificMicroState in totalMicroStates
+        result = any(all(all(is_close(a, b) for a, b in zip(subarray_2d, input_array_2d)) for subarray_2d, input_array_2d in zip(subarray, datCreatedMicroState[0])) for subarray in totalMicroStates if subarray) and datCreatedMicroState[0] in totalMicroStates
         if previousi == i:
             iCounter += 1
         else:
@@ -93,10 +97,10 @@ def calcPossibleConfig(m,vElectron, limit):
         else:
             if i < len(totalMicroStates):
                 print(f"generating: {i}")
-                totalMicroStates[i] = specificMicroState
+                totalMicroStates[i] = datCreatedMicroState[0]
             else:
                 print(f"generating: {i}")
-                totalMicroStates.append(specificMicroState)  # Append specificMicroState if i is beyond the current length
+                totalMicroStates.append(datCreatedMicroState[0])  # Append specificMicroState if i is beyond the current length
             i += 1
             continue
     print(totalMicroStates)
@@ -107,28 +111,45 @@ def calculateMsMl(m,totalMicrostate):
     MicroStateConfigurationList = [[[],[]] for _ in range(len(totalMicrostate))]
 
     convertedMList = list(range(-(m//2), m//2 + 1))
+    print(convertedMList)
     for i in range(0,len(totalMicrostate)):
         Ms = 0
         Ml = 0
         
-        for j in totalMicrostate[i]:
+        #jCounter = 0
+        for jCounter, j in enumerate(totalMicrostate[i]):
             for k in j:
+                
                 Ms += k
                 if k != 0:
-                    Ml += convertedMList[totalMicrostate[i].index(j)]
+                    Ml += convertedMList[jCounter] # there are multiple in convertedMList that have the same index j
+            #jCounter +=1
         MicroStateConfigurationList[i][0] = Ms
         MicroStateConfigurationList[i][1] = float(Ml)
     return MicroStateConfigurationList
 
+def TermTable(data):
+    table = {}
+    for microStateConfig in data:
+        column = microStateConfig[1]
+        value = microStateConfig[0]
 
+        if column not in table:
+            table[column] = []
+        table[column].append(value)
+    return table
 
+if __name__ == "__main__":
+    insertedM = 5
+    start = time.time()
+    totalMicroStateReturned = calcPossibleConfig(insertedM,5,5000) # do systematic amount of mixing go over every microstate do x times mixing for all configurations decrease the amount of random
+    end = time.time()
+    print(end-start)
 
-start = time.time()
-totalMicroStateReturned = calcPossibleConfig(7,7,25000) # do systematic amount of mixing go over every microstate do x times mixing for all configurations decrease the amount of random
-end = time.time()
-print(end-start)
-with open("output.txt", "w") as f:
-    f.write("\n".join(str(item) for item in totalMicroStateReturned))
+    with open("output.txt", "w") as f:
+        f.write("\n".join(str(item) for item in totalMicroStateReturned))
 
-print(calculateMsMl(3,totalMicroStateReturned))
-# can solve americum in 143.8940735s (7,7,20000)
+    calcMsMl = calculateMsMl(insertedM,totalMicroStateReturned)
+    print(TermTable(calcMsMl))
+
+    # can solve americum in 131.06236481666565 s (7,7,20000)
---
+++ test.py
@@ -0,0 +1,5 @@
+list_ = [9,8,7]
+
+for jcount,i in enumerate(list_):
+    print(jcount)
+    print(i)
